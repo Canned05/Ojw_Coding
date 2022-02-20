13a7423384c95721d1d4d520f97dfe5969d40b3f
Canned05 <87993453+Canned05@users.noreply.github.com>
2022-02-20 11:51:13 +0900

    class update

--- platformer/platformer_tut1.py
+++ platformer/platformer_tut1.py
@@ -1,10 +1,43 @@
 import pygame
 
+class Map:
+	def __init__(self , map):
+		self.map = map
+		dirt_img = pygame.image.load("img/dirt.png")
+		grass_img = pygame.image.load("img/grass.png")
+
+		self.dirt_img = pygame.transform.scale(dirt_img ,(50,50))
+		self.grass_img = pygame.transform.scale(grass_img ,(50,50))
+
+	def update(self,SCR):
+		for i,row in enumerate(self.map): # row -> [0,0,0,0,...0]
+			for j, col in enumerate(row): # col -> 0 or 1
+				print(j,col)
+				if col == 1:
+					SCR.blit(self.grass_img,(50*j,50*i))
+				elif col == 2:
+					SCR.blit(self.dirt_img,(50*j,50*i))
+					
+class Player:
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+		self.img = pygame.image.load("img/guy1.png")
+	
+	def update(self , screen):
+		key = pygame.key.get_pressed()
+		if key[pygame.K_LEFT]:
+			self.x -= 5
+		elif key[pygame.K_RIGHT]:
+			self.x += 5
+		screen.blit(self.img , (self.x,self.y))
+		
+
 
 pygame.init()
 
-screen_width = 500
-screen_height = 500
+screen_width = 1000
+screen_height = 1000
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('오정우')
@@ -12,22 +45,74 @@ pygame.display.set_caption('오정우')
 sky_img = pygame.image.load("img/sky.png")
 sun_img = pygame.image.load("img/sun.png")
 
+player_img = pygame.image.load("img/guy1.png")
+
+player_x = 100
+player_y = 100
+
+#dirt_img = pygame.image.load("img/dirt.png")
+#grass_img = pygame.image.load("img/grass.png")
+
+#dirt_img = pygame.transform.scale(dirt_img ,(50,50))
+#grass_img = pygame.transform.scale(grass_img ,(50,50))
+
+world_map = [
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2],
+[1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+
+]
+
+map = Map(world_map)
+player = Player(200,200)
+
 run = True  
 while run:
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
-
+	'''
 	key = pygame.key.get_pressed()
 	if key[pygame.K_LEFT]:
-		print("left")
+		player.x -= 5
 	elif key[pygame.K_RIGHT]:
-		print("right")
+		player.x += 5
+	'''
 	
 
 	screen.blit(sky_img,(0,0))
 	screen.blit(sun_img,(10,10))
+	map.update(screen)
+	player.update(screen)
+	#screen.blit(player.img , (player.x,player.y))
+
+	'''
+	for i,row in enumerate(world_map): # row -> [0,0,0,0,...0]
+		for j, col in enumerate(row): # col -> 0 or 1
+			print(j,col)
+			if col == 1:
+				screen.blit(self.grass_img,(50*j,50*i))
+			elif col == 2:
+				screen.blit(self.dirt_img,(50*j,50*i))
+	'''
 	pygame.display.update()
 
 pygame.quit()
